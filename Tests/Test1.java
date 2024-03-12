d98abcb444a32fc97e1bec486c755a151778bc81
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2024-03-12 12:38:00 +0100

    abstracting tests

---
+++ Tests/Test1.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import colors.Colors;
+import static src.Tools.*;
+import static styles.Styles.*;
+import static input.Input.*;
+
+public class Test1 {
+    Test1(){
+        print(Colors.CYAN_BRIGHT, "Demetrio ::");
+        print(Colors.CYAN_BRIGHT, "un uomo ");
+        setText_color(Colors.BLUE_BOLD);
+        println("Demetrio -_-");
+        println(Colors.RED_BACKGROUND_BRIGHT, "Ekavkavdfhad aiyfa");
+        print(getNextLine());
+        print(Colors.RED, "wwwww ");
+        println("io sono blu ");
+        println(Colors.GREEN_BOLD, "sssss");
+        resetText_color();
+        println("io sono normale"); println(Colors.GREEN_BACKGROUND, "io");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,10 @@
 package src;
 
-import colors.Colors;
-import static src.Tools.*;
-import static styles.Styles.*;
-import static input.Input.*;
+import Tests.Test1;
 
 public class Main {
     public static void main(String[] args) {
-        print(Colors.CYAN_BRIGHT, "Demetrio ::");
-        print(Colors.CYAN_BRIGHT, "un uomo ");
-        setText_color(Colors.BLUE_BOLD);
-        println("Demetrio -_-");
-        println(Colors.RED_BACKGROUND_BRIGHT, "Ekavkavdfhad aiyfa");
-        print(getNextLine());
-        print(Colors.RED, "wwwww ");
-        println("io sono blu ");
-        println(Colors.GREEN_BOLD, "sssss");
-        resetText_color();
-        println("io sono normale");
+        new Test1();
     }
 
 }
