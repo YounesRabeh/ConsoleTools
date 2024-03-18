2536e9782733949a6dcc147c45c12a0031ac6410
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2024-03-18 12:54:43 +0100

    working table drawing V1.1

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ import static src.Tools.*;
 import static structures.tables.TableTypes.LIGHT;
 
 import colors.Colors;
+import structures.tables.TableTypes;
+
 import static input.Input.*;
 
 public class Main {
@@ -15,13 +17,19 @@ public class Main {
         String askLength = "Enter data:  ";
         String askRows = "Enter the number of row: ";
         String askColumns = "Enter the number of columns: ";
+        String asType = "Enter the type: ";
+
         print(Colors.CYAN_BRIGHT, askRows);
         int rowNum = Integer.parseInt(getNextLine());
         print(Colors.CYAN_BRIGHT, askColumns);
         int colNum = Integer.parseInt(getNextLine());
+        print(Colors.GREEN_BOLD, asType);
+        TableTypes typeNum = TableTypes.valueOf(String.valueOf(getNextLine()));
         print(Colors.RED_BRIGHT, askLength);
         Object data = getNextLine();
-        Table table = new Table(LIGHT, rowNum, colNum);
+
+
+        Table table = new Table(typeNum, rowNum, colNum);
         table.setField(0, 0,data);
         table.setField(0, 1, askLength);
 
--- styles/TableStyles.java
+++ styles/TableStyles.java
@@ -4,8 +4,22 @@ import structures.tables.TableTypes;
 
 public final class TableStyles {
     private static final String[] LIGHT_BORDERS = {"─", "│", "┌", "┬", "┐\n", "├", "┼", "┤\n", "└", "┴", "┘\n"};
+    private static final String[] BOLD_BORDERS = {"━", "┃", "┏", "┳", "┓\n", "┣", "╋", "┫\n", "┗", "┻", "┛"};
+    private static final String[] DOUBLE_LINED_BORDERS = {"═", "║", "╔", "╦", "╗\n", "╠", "╬", "╣\n", "┗" +
+            "╚", "╩", "╝"};
+
     public static String[] getTableStyle(TableTypes type){
-        return LIGHT_BORDERS;
+        switch (type){
+            case BOLD -> {
+                return BOLD_BORDERS;
+            }
+            case DOUBLE_LINED -> {
+                return DOUBLE_LINED_BORDERS;
+            }
+            default -> {
+                return LIGHT_BORDERS;
+            }
+        }
 
     }
 }
